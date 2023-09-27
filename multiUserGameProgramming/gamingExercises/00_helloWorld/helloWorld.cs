6a89db884f0a6886842e69787c11433f57b09ac1
alex <90472869+alexscott06@users.noreply.github.com>
2023-09-27 12:32:02 -0400

    v0.0

---
+++ multiUserGameProgramming/gamingExercises/00_helloWorld/helloWorld.cs
@@ -0,0 +1,61 @@
+// Alex Scott, Hello World, v0.5
+using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            string myString1 = "string1";
+            string myString2 = "string2";
+
+            int myInt1 = 1;
+            int myInt2 = 2;
+
+            float myFloat1 = 1.0F;
+            float myFloat2 = 2.0F;
+
+            // Arithemic Operators
+            Console.WriteLine(myInt1 + myInt2);
+            Console.WriteLine(myString1 + myString2);
+            Console.WriteLine(myFloat1 + myFloat2);
+            Console.WriteLine(myInt1 + myFloat2);
+
+            Console.WriteLine(myInt1 - myInt2);
+            // Console.WriteLine(myString1 - myString2); DOES NOT APPLY TO STRINGS
+            Console.WriteLine(myFloat1 - myFloat2); 
+            Console.WriteLine(myInt1 - myFloat2);
+
+            Console.WriteLine(myInt1 / myInt2);
+        }    
+    }
+}
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+`
+
+
+ 
+
+
+
+
+
+
+
+
---
+++ multiUserGameProgramming/gamingExercises/00_numberGuess/numberGuess.cs
@@ -0,0 +1,35 @@
+// Alex Scott, Program Template, v0.02
+/*
+Generate secret number from a defined range of numbers (i.e. 0-10, 0-50, 0-100)
+Print game instructions including range and num. of guesses allowed. 
+    MATCH == first player to score 3 points.
+    ROUND == guessing a specific number, until correct or no more attempts. 
+
+Ask the player what their guess is.
+Determine if guess is correct or not.
+    If guess is correct {
+        Tell them they have guessed correctly.
+        Award the player a point. 
+    } else {
+        Tell them they are wrong. 
+        Tell player if the guess is too high or too low
+        Check to see if they have guesses remaining }
+            if guess remain: {
+                allow player to guess again.
+
+            }
+*/
+using System;
+
+namespace numberGuess
+{
+    0 references
+    class numberGuess
+    {
+        0 references 
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

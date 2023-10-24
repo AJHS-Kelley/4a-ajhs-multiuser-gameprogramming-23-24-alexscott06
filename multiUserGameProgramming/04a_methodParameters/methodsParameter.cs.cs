890ddb0e0cc96de66beeaa532135778f4fc3007a
alex <90472869+alexscott06@users.noreply.github.com>
2023-10-24 14:11:29 -0400

    v0.1

---
+++ multiUserGameProgramming/04a_methodParameters/methodsParameter.cs.cs
@@ -0,0 +1,22 @@
+// Alex Scotts, Program Template, v0.1
+using System;
+
+namespace UpdateForEachProgram
+{
+
+    class UpdateForEachProgram
+    {    
+        // METHOD -- named block of code, can be used over and over.
+        // All methods have a SIGNATURE that defines their name, parameters, and output.
+        // Example Signature
+        static void MyMethod() 
+        {
+            Console.WriteLine("I love pizza.\n");
+        }
+       
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}

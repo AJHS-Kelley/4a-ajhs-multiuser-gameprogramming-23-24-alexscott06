607f310041a56e38c7f71a46159cd4159aa7d9a3
alex <90472869+alexscott06@users.noreply.github.com>
2023-11-14 14:54:56 -0500

    Create exampleGameMethods.cs

---
+++ multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
@@ -0,0 +1,41 @@
+// Alex Scott, Program Template, v0.01
+using System; 
+// Making platformer called Key 
+// Difficulty of game is easy, normal hard
+// Playable character is named Code 
+// Currency of game are called keys and once you get 100 keys, they gain a special key that gives them acccess to the next level.
+// 2D platformer 
+// If Code gets damaged a number of three times, he loses 50 keys and if the player has already gained the special key, they lose it and still remain with 50 keys lost. 
+// Code gets attacked by obstacles like shadow monsters and flying weapons. 
+
+namespace UPDATEFOREACHPROGRAM 
+{
+    class UPDATEFOREACHPROGRAM 
+    {
+        static void Method()
+        {
+            
+        }
+
+        static void Method()
+        {
+
+        }
+
+        static void Method()
+        {
+
+        }
+
+        static void Method()
+        {
+
+        }
+
+        static void Mai(string[] args)
+        {
+            
+        }
+    }
+}
+ 
